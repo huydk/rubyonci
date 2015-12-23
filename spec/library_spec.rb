08b02f42c6e01c5c2aeeb86bef23fa58863e0073
Huy Nguyen <huydongkhe@gmail.com>
2015-12-23 12:39:30 +0700

    dev1: i pulled code from dev2 branch and modified something

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -32,5 +32,22 @@ describe "Library object" do
             @lib.should have(5).books
         end
     end
-  end 
+  end
+  
+  it "returns all the books in a given category" do
+    @lib.get_books_in_category(:development).length.should == 2
+  end
+ 
+  it "accepts new books" do
+    @lib.add_book( Book.new("Designing for the Web", "Mark Boulton", :design) )
+    @lib.get_book("Designing for the Web").should be_an_instance_of Book
+  end
+ 
+  it "saves the library" do
+    books = @lib.books.map { |book| book.title }
+    @lib.save
+    lib2 = Library.new "books.yml"
+    books2 = lib2.books.map { |book| book.title }
+    books.should eql books2
+  end
 end
